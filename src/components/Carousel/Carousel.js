4738bac5f7ce9655411c2f808ed5740400cd5c28
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-07-17 23:32:01 +0530

    Update Carousel.js

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import img1 from '../../assets/home.jpg'
+import img1 from '../../assets/home.jpg';
 
 const Carousel = () => {
 
-const MaintextStyle={
-    
+  const MaintextStyle = {
     fontWeight: 'bold',
     textShadow: '2px 2px 4px rgba(0, 0, 0, 1)',
-    WebkitTextStrokeWidth: '0.5px',
+    WebkitTextStrokeWidth: '0.35px',
     WebkitTextStrokeColor: 'white',
-    paddingTop:'0px',
-    
-}
+    paddingTop: '0px',
+  };
 
-const textStyle={
-    
-  fontWeight: 'bold',
-  paddingTop:'0px',
-  
-  
-}
+  const textStyle = {
+    fontWeight: 'bold',
+    paddingTop: '0px',
+    fontSize: '40px',
+    paddingTop: '40px',
+  };
+
   return (
     <div className="container-fluid p-0 pb-5">
       <div className="owl-carousel header-carousel position-relative mb-5">
@@ -29,19 +27,14 @@ const textStyle={
             <div className="container">
               <div className="row justify-content-start">
                 <div className="col-10 col-lg-8">
-                  
-                  <h1 className="display-3 text-primary animated slideInDown mb-4" 
-                                      style={MaintextStyle}>UNIVERSAL <span className="text-secondary" >FREIGHT SOLUTIONS LLC</span></h1>
-                  <h3 className="display-3 text-white  mb-4" style={textStyle}>#1 Place For Your Logistics Solution need</h3>
-                  
+                  <h1 className="display-3 text-primary animated slideInDown mb-4" style={MaintextStyle}>UNIVERSAL <span className="text-secondary">FREIGHT SOLUTIONS LLC</span></h1>
+                  <h3 className="display-3 text-white mb-4" style={textStyle}>#1 Place For Your Logistics Solution need</h3>
                   <a href="" className="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Read More</a>
-                  
                 </div>
               </div>
             </div>
           </div>
         </div>
-        
       </div>
     </div>
   );
