555b6a6eb51d3f44a5c0ac492a4dcf2ee2258e2b
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-07-18 14:20:22 +0530

    all

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -9,12 +9,13 @@ const Carousel = () => {
     WebkitTextStrokeWidth: '0.35px',
     WebkitTextStrokeColor: 'white',
     paddingTop: '0px',
+    
   };
 
   const textStyle = {
     fontWeight: 'bold',
     paddingTop: '0px',
-    fontSize: '40px',
+    
     paddingTop: '40px',
   };
 
@@ -23,14 +24,20 @@ const Carousel = () => {
       <div className="owl-carousel header-carousel position-relative mb-5">
         <div className="owl-carousel-item position-relative">
           <img className="img-fluid" src={img1} alt="" />
-          <div className="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style={{ background: 'rgba(6, 3, 21, .5)' }}>
+          <div className="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style={{ background: 'rgba(1, 5, 20, .6)' }}>
             <div className="container">
               <div className="row justify-content-start">
-                <div className="col-10 col-lg-8">
-                  <h1 className="display-3 text-primary animated slideInDown mb-4" style={MaintextStyle}>UNIVERSAL <span className="text-secondary">FREIGHT SOLUTIONS LLC</span></h1>
-                  <h3 className="display-3 text-white mb-4" style={textStyle}>#1 Place For Your Logistics Solution need</h3>
-                  <a href="" className="btn btn-primary py-md-3 px-md-5 me-3 animated slideInLeft">Read More</a>
+                <div className='wow animate__animated animate__fadeInUp'>
+                <h3 className="display-3 text-primary animated slideInDown mb-4 " style={MaintextStyle}>UNIVERSAL <span className="text-secondary">FREIGHT SOLUTIONS LLC</span></h3>
+                  <h5 className="display-3 text-white animated slideInDown mb-4" style={textStyle}>#1 Place For Your Logistics Solution need</h5>
+                  
+                  <div className="col-10 col-lg-8">
+                  <a href="" className="btn btn-primary py-md-3 px-md-5 me-3">Read More</a>
                 </div>
+                </div>
+                  
+
+
               </div>
             </div>
           </div>
