ebeeeea5487bbe48814227c8ba4a711a25b0fa49
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-07-18 14:54:44 +0530

    all

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import img1 from '../../assets/home.jpg';
+import './Carousel.css'
 
 const Carousel = () => {
 
@@ -23,27 +24,30 @@ const Carousel = () => {
     <div className="container-fluid p-0 pb-5">
       <div className="owl-carousel header-carousel position-relative mb-5">
         <div className="owl-carousel-item position-relative">
-          <img className="img-fluid" src={img1} alt="" />
+        <img className="img-fluid" src={img1} style={{ width: '100%',  }} alt="" />
+
           <div className="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style={{ background: 'rgba(1, 5, 20, .6)' }}>
             <div className="container">
               <div className="row justify-content-start">
                 <div className='wow animate__animated animate__fadeInUp'>
-                <h3 className="display-3 text-primary animated slideInDown mb-4 " style={MaintextStyle}>UNIVERSAL <span className="text-secondary">FREIGHT SOLUTIONS LLC</span></h3>
-                  <h5 className="display-3 text-white animated slideInDown mb-4" style={textStyle}>#1 Place For Your Logistics Solution need</h5>
+                
+                <div class="container text-center py-5">
+            <h1 class="text-primary mb-4">Safe & Faster</h1>
+            <h1 class="text-white display-3 mb-5">Logistics Services</h1>
+            <h3 className="display-3 text-primary animated slideInDown mb-4 " style={MaintextStyle}>UNIVERSAL <span className="text-secondary">FREIGHT SOLUTIONS LLC</span></h3>
+            <div class="mx-auto" style={{ width: '100%', maxWidth: '600px' }}>
+               
+            </div>
+        </div>
                   
-                  <div className="col-10 col-lg-8">
-                  <a href="" className="btn btn-primary py-md-3 px-md-5 me-3">Read More</a>
                 </div>
                 </div>
-                  
-
-
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    
   );
   
   }
