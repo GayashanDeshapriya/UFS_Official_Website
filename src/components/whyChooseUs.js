f5cb2e054f7d181364d63fe7d5c4623cacbcf602
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-07-20 23:18:33 +0530

    Update whyChooseUs.js

--- src/components/whyChooseUs.js
+++ src/components/whyChooseUs.js
@@ -11,7 +11,7 @@ function Chooseus() {
             <div className="col-lg-7 py-5 py-lg-0">
               <h6 className="text-primary text-uppercase font-weight-bold">Why Choose Us</h6>
               <h1 className="mb-4 text-secondary">Faster, Safe and Trusted Logistics Services</h1>
-              <p className="mb-4">Dolores lorem lorem ipsum sit et ipsum. Sadip sea amet diam dolore sed et. Sit rebum labore sit sit ut vero no sit. Et elitr stet dolor sed sit et sed ipsum et kasd ut. Erat duo eos et erat sed diam duo</p>
+              <p className="mb-4"></p>
               <ul className="list-inline text-primary-second">
                 <li><h6><i className="far fa-dot-circle text-primary mr-3"></i>   Experience Staff</h6></li>
                 <li><h6><i className="far fa-dot-circle text-primary mr-3"></i>   Unique Approach</h6></li>
