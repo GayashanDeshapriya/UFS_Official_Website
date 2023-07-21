5a5f921fe21c831ace7bd74bd2b55e8450736589
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-07-21 23:04:38 +0530

    Update Carousel.js

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -37,7 +37,7 @@ const Carousel = () => {
                     <div class="container text-center py-1">
 
                       <h2 className="display-3 text-primary  mb-4 " style={MaintextStyle}>UNIVERSAL <span className="text-secondary">FREIGHT SOLUTIONS LLC</span></h2>
-                      <h4 class="text-white text-uppercase mb-5 animated slideInDown" style={textStyle}>#1 Place For Your Freight & Logistics need</h4>
+                      <h4 class="text-white text-uppercase mb-5 animated slideInDown" style={textStyle}>#1 Place For Your Freight & Logistics needs</h4>
                       <div class="mx-auto" style={{ width: '100%', maxWidth: '600px' }}>
 
                       </div>
