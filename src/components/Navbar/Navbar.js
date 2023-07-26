719135ca6357dc77bfece685a07fa387f0156a1a
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-07-26 08:09:27 +0530

    Update Navbar.js

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -34,10 +34,10 @@ function Navbar() {
   return (
     <nav className={`navbar navbar-expand-lg bg-white navbar-light shadow border-top border-5 border-primary ${fix ? 'fixed-top' : ''} p-0`}>
       <a href="/" className="navbar-brand d-flex align-items-center px-4 px-lg-5" onClick={scrollToTop}>
-        <img src={logo} alt="logo" width="160" height="auto" />
+        <img src={logo} alt="logo" width="150" height="auto" />
       </a>
-      <button type="button" className="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
-        <span className="navbar-toggler-icon"></span>
+      <button type="button" className="text-primary navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+        <span className="navbar-toggler-icon "></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarCollapse">
         <div className="navbar-nav ms-auto p-4 p-lg-0">
