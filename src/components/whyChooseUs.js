ddbc1caccc67a23f835230d1a23fe1b91612704e
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-07-28 20:48:19 +0530

    Update whyChooseUs.js

--- src/components/whyChooseUs.js
+++ src/components/whyChooseUs.js
@@ -5,10 +5,10 @@ function Chooseus() {
     <div className="container-fluid bg-light my-5 wow animate__animated animate__fadeInUp" data-wow-offset="250">
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-lg-5">
-            <img src={img3} className="img-fluid w-100" alt="" />
+          <div className="col-lg-8">
+            <img src={img3} style={{width:''}} className="img-fluid w-100" alt="" />
           </div>
-          <div className="col-lg-7 py-5 py-lg-0">
+          <div className="col-lg-4 py-5 py-lg-0">
             <h6 className="text-primary text-uppercase font-weight-bold">Why Choose Us</h6>
             <h1 className="mb-4 text-secondary">Fast, Safe and Trusted Logistics Services</h1>
             <p className="mb-4"></p>
