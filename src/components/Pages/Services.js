3fa93d14a23eda6ce00f0b7c89cad3c8439b2ccc
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-07-28 21:01:03 +0530

    Update Services.js

--- src/components/Pages/Services.js
+++ src/components/Pages/Services.js
@@ -10,11 +10,10 @@ function Services() {
   return (
     <div>
 
-
       <div className="container-xxl py-5">
         <div className="container py-5">
           <div className="text-center wow fadeInUp" data-wow-delay="0.10s">
-            
+
             <h5 className="text-primary text-uppercase mb-2">Explore Our Services</h5>
             <h2 className="mb-5 text-primary font-weight-bold" style={{ fontWeight: 'bold' }}>UNIVERSAL <span className="text-secondary" >FREIGHT SOLUTIONS LLC</span></h2>
           </div>
@@ -40,14 +39,15 @@ function Services() {
                   <li>	Air Charter services</li>
                 </ul>
 
-
-
-
               </div>
             </div>
             <div
               className="col-md-6 col-lg-4 wow animate__animated animate__fadeInUp"
               data-wow-delay="0.5s"
+
+
+
+
             >
               <div className="service-item p-4">
                 <div className="overflow-hidden mb-4">
