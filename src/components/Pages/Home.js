c32061299662915c00e0bf8d07f23bf1d2a26f43
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-08-10 20:32:26 +0530

    update

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,27 +1,42 @@
-import React from 'react'
-import Carousel from '../Carousel/Carousel'
-import ServicesHome from './ServicesHome'
-import Chooseus from '../whyChooseUs'
-
-
+import React, { useState, useEffect } from 'react';
+import Carousel from '../Carousel/Carousel';
+import ServicesHome from './ServicesHome';
+import Chooseus from '../whyChooseUs';
 
 function Home() {
+  const [carouselLoaded, setCarouselLoaded] = useState(false);
+  const [servicesLoaded, setServicesLoaded] = useState(false);
+  
 
+  useEffect(() => {
+    // Simulate the loading process (you can replace setTimeout with actual data loading)
+    const carouselLoadTimeout = setTimeout(() => {
+      setCarouselLoaded(true);
+    }, 0); // Adjust the time as needed
 
+    return () => clearTimeout(carouselLoadTimeout);
+  }, []);
+
+  useEffect(() => {
+    if (carouselLoaded) {
+      // Simulate the loading process (you can replace setTimeout with actual data loading)
+      const servicesLoadTimeout = setTimeout(() => {
+        setServicesLoaded(true);
+      }, 500); // Adjust the time as needed
+
+      return () => clearTimeout(servicesLoadTimeout);
+    }
+  }, [carouselLoaded]);
+  
 
   return (
-
     <div>
-      <Carousel />
-      <ServicesHome />
-      <Chooseus />
-
+      {carouselLoaded ? <Carousel /> : <p></p>}
+      {servicesLoaded ? <ServicesHome /> : <p></p>}
+      {servicesLoaded ? <Chooseus /> : null}
     </div>
-
-
   );
-
-
 }
 
-export default Home
+export default Home;
+
