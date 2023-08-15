455ddcac75ce19f1d32a7c51ca7bf94eec8b0d2f
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-08-15 16:23:26 +0530

    all

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -25,8 +25,6 @@ const Carousel = () => {
       <div className="owl-carousel header-carousel position-relative mb-5">
         <div className="owl-carousel-item position-relative">
           <div className='bg-image'>
-
-
             <div className="img-container">
               <LazyLoadImage
                 effect="blur"
@@ -36,25 +34,11 @@ const Carousel = () => {
                 style={{ width: '100%' }}
               />
             </div>
-
-
-
             <div className="position-absolute top-0 start-0 w-100 h-100 d-flex align-items-center" style={{ background: 'rgba(25, 4, 20, .5)' }}>
-
               <div className="container">
                 <div className='wow animate__animated animate__fadeInUp animate__delay-1s'>
                   <div className="row justify-content-start">
-
-
-                    <div class="container text-center py-1">
-
-                      <h2 className="display-3 text-primary  mb-4 " style={MaintextStyle}>UNIVERSAL <span className="text-secondary">FREIGHT SOLUTIONS LLC</span></h2>
-                      <h4 class="text-primary text-uppercase mb-5 animated slideInDown" style={textStyle}>one stop for all your Freight & Logistics needs</h4>
-                      <div class="mx-auto" style={{ width: '100%', maxWidth: '600px' }}>
-
-                      </div>
-                    </div>
-
+                    {/* Content Goes Here */}
                   </div>
                 </div>
               </div>
@@ -63,8 +47,8 @@ const Carousel = () => {
         </div>
       </div>
     </div>
-
   );
+  
 
 }
 export default Carousel;
