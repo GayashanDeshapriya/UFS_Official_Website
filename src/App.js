b00bb8f0620b096076a9e309def2728aba6d7e9c
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-08-15 14:52:24 +0530

    Add code Splitting

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
-import { Routes , Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Topbar from './components/Topbar/Topbar';
 import Footer from './components/Footer/Footer';
-import Home from './components/Pages/Home';
-import About from './components/Pages/About';
-import Services from './components/Pages/Services';
-import Contact from './components/Pages/Contact';
+//import Home from './components/Pages/Home';
+//import About from './components/Pages/About';
+//import Services from './components/Pages/Services';
+//import Contact from './components/Pages/Contact';
+import React, { lazy, Suspense } from 'react';
+
+const Home = lazy(() => wait(500).then(()=> import("./components/Pages/Home")))
+const About = lazy(() => wait(500).then(()=> import("./components/Pages/About")))
+const Services = lazy(() => wait(500).then(()=> import("./components/Pages/Services")))
+const Contact = lazy(() => wait(500).then(()=> import("./components/Pages/Contact")))
+
 
 
 function App() {
 
-  return(
+  return (
     <>
-    <Topbar/>
-    <Navbar/>
+      <Topbar />
+      <Navbar />
 
-    <div>
-      <Routes>
+      <Suspense fallback={<h1 className="mb-5 text-primary font-weight-bold" align="center" style={{ fontWeight: 'bold' }}>Loading....</h1>}>
+        <Routes>
 
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/service" element={<Services/>}/>
-        <Route path="/contact" element={<Contact/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
 
-      </Routes>
-    
-     <Footer/>
-       
-    </div>
-    
-    
-</>
+        </Routes>
+      </Suspense>
+
+
+      <Footer />
+
+
+
+
+    </>
   );
-  
+
+}
+function wait(time){
+  return new Promise(resolve => setTimeout(resolve, time))
 }
 
 export default App;
