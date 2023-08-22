9aa09585f2e0b10089808a46f52e623b4a6efffc
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-08-22 19:31:15 +0530

    all

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -4,6 +4,9 @@ import img2 from '../../assets/home1.png';
 import img3 from '../../assets/home6.png';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css'; // Import the blur effect CSS
+import styled from "styled-components";
+
+
 
 
 
@@ -25,40 +28,49 @@ const Carousel = () => {
   const opacity = 0;
 
   return (
-    <div className="container-fluid position-relative p-0">
-      <div
-        className="container-fluid py-1 mb-3 hero-header loading='lazy'"
-        style={{
-          backgroundImage: `url(${img3})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'right',
-          width: '100%',
-          height: '600px',
-          backgroundColor: `rgba(0, 0, 0, ${opacity})`,
-        }}
-      >
-        <div className="container py-1">
-          <div className="row justify-content-left py-1">
-            <div className="col-lg-8 pt-lg-4 mt-lg-5 text-left">
-              <h1
-                className="display-3 text-primary mb-1 animated slideInDown"
-                style={maintextStyle}
-              >
-                UNIVERSAL <span className="text-secondary">FREIGHT SOLUTIONS LLC</span>
-              </h1>
-              <h3 className="display-3 text-primary mb-4" style={textStyle}>
-                #1 Place For Your Logistics Solution need
-              </h3>
+
+    <header class="py-5">
+      <div class="container px-5 pb-5">
+        <div class="row gx-5 align-items-center">
+          <div class="col-xxl-5">
+
+            <div class="text-center text-xxl-start">
+             
+              
+              <h1 class="display-3 fw-bolder mb-6 text-primary animated slideInDown"><span class="text-gradient d-inline">UNIVERSAL <span className="text-secondary">FREIGHT SOLUTIONS LLC</span></span></h1>
+              <div class="fs-3 fw-light  text-primary">#1 Place For Your Logistics Solution need</div>
+              <div class="d-grid gap-1 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-2">
+
+              </div>
+            </div>
+          </div>
+          <div class="col-xxl-7">
+
+            <div class="d-flex justify-content-center mt-5 mt-xxl-0">
+              <div class="profile bg-gradient-primary-to-secondary">
+
+                <div className='bg-image'>
+                  <div className="img-container col-13  mb-1">
+                    <LazyLoadImage
+                      effect="blur"
+                      src={img2}
+                      alt=""
+                      className="img-fluid"
+                      style={{ width: '800px', float: 'right' }}
+                    />
+
+                  </div>
+
+                  </div>
+
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+    </header>
   );
-  
-  
- 
-
-
 }
+
 export default Carousel;
+
