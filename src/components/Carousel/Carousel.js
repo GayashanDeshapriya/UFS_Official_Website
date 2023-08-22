324aa7c43a3e2e05414a6b31908ef45feb848500
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-08-22 21:04:36 +0530

    all

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import img1 from '../../assets/home.jpg';
-import img2 from '../../assets/home1.png';
-import img3 from '../../assets/home6.png';
+import img2 from '../../assets/home1.svg';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css'; // Import the blur effect CSS
-import styled from "styled-components";
-
-
-
+import 'react-lazy-load-image-component/src/effects/blur.css';
+import './Carousel.css';
 
 
 const Carousel = () => {
@@ -27,16 +23,22 @@ const Carousel = () => {
 
   const opacity = 0;
 
+  const headerStyle = {
+    backgroundImage: `url(${img1})`, // Set the image as background
+    backgroundSize: 'cover', // Adjust background size to cover the header
+    backgroundPosition: 'center', // Center the background image
+  };
+
   return (
 
-    <header class="py-5">
+    <header class="py-5 " >
       <div class="container px-5 pb-5">
         <div class="row gx-5 align-items-center">
           <div class="col-xxl-5">
 
             <div class="text-center text-xxl-start">
-             
-              
+
+
               <h1 class="display-3 fw-bolder mb-6 text-primary animated slideInDown"><span class="text-gradient d-inline">UNIVERSAL <span className="text-secondary">FREIGHT SOLUTIONS LLC</span></span></h1>
               <div class="fs-3 fw-light  text-primary">#1 Place For Your Logistics Solution need</div>
               <div class="d-grid gap-1 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-2">
@@ -48,8 +50,8 @@ const Carousel = () => {
 
             <div class="d-flex justify-content-center mt-5 mt-xxl-0">
               <div class="profile bg-gradient-primary-to-secondary">
-
-                <div className='bg-image'>
+                <div>
+                <div className='bg-image bounce-2'>
                   <div className="img-container col-13  mb-1">
                     <LazyLoadImage
                       effect="blur"
@@ -61,13 +63,15 @@ const Carousel = () => {
 
                   </div>
 
-                  </div>
-
                 </div>
+                </div>
+               
+
               </div>
             </div>
           </div>
         </div>
+      </div>
     </header>
   );
 }
