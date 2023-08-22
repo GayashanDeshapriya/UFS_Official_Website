d33ca6532387f8dc28d0f2463bd10f441ec378c4
GayashanDeshapriya <gayashandeshapriya1922@gmail.com>
2023-08-22 22:18:23 +0530

    all

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import img1 from '../../assets/home.jpg';
+import img1 from '../../assets/Picture1.jpg';
 import img2 from '../../assets/home.svg';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
@@ -31,16 +31,17 @@ const Carousel = () => {
 
   return (
 
-    <header class="py-5 " >
+    <header class="py-5 mb-5  linear-gradient" >
+     
       <div class="container px-5 pb-5">
         <div class="row gx-5 align-items-center">
           <div class="col-xxl-5">
 
             <div class="text-center text-xxl-start">
-
-
-              <h1 class="display-3 fw-bolder mb-6 text-primary animated slideInDown"><span class="text-gradient d-inline">UNIVERSAL <span className="text-secondary">FREIGHT SOLUTIONS LLC</span></span></h1>
-              <div class="fs-3 fw-light  text-primary">#1 Place For Your Logistics Solution need</div>
+             
+              
+              <h1 class="display-1 fw-bolder mb-6 text-primary animated slideInDown text-outline"><span class="text-gradient d-inline">UNIVERSAL <span className="text-secondary">FREIGHT SOLUTIONS LLC</span></span></h1>
+              <div class="fs-3 fw-light  text-white">#1 Place For Your Logistics Solution need</div>
               <div class="d-grid gap-1 d-sm-flex justify-content-sm-center justify-content-xxl-start mb-2">
 
               </div>
@@ -50,8 +51,8 @@ const Carousel = () => {
 
             <div class="d-flex justify-content-center mt-5 mt-xxl-0">
               <div class="profile bg-gradient-primary-to-secondary">
-                <div>
-                <div className='bg-image bounce-2'>
+
+                <div className='bg-image'>
                   <div className="img-container col-13  mb-1">
                     <LazyLoadImage
                       effect="blur"
@@ -63,15 +64,13 @@ const Carousel = () => {
 
                   </div>
 
-                </div>
-                </div>
-               
+                  </div>
 
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
     </header>
   );
 }
